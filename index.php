27d0a2d67b6a15ce63b522314dcdd6725faaa14a
Henrique Stedile <stedile@outlook.com>
2017-06-30 13:07:04 -0300

    melhoria index.php

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 
 require 'Psr4Autoloader.php';
 use entity\Usuario;
+use entity\Produto;
 use database\DatabaseConnection;
 
-$user = new Usuario("email@extensao.com", "senha");
-$user->salvar(DatabaseConnection::getConnection());
+$conexao = DatabaseConnection::getConnection();
+
+$usuario = new Usuario("email@extensao.com", "senha");
+$usuario->salvar($conexao);
+
+$produto = new Produto("Coca-Cola 2,5Lt.", 4.80);
+$produto->salvar($conexao);
+
+
+
 
 
 
